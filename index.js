549cb7296d8fcd231591e624edf806ea8d3676a0
Tev-J <tj_delph@yahoo.com>
2023-04-24 11:19:22 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  const post = fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: `${userName}`,
+      email: `${userEmail}`,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => handleRes(data.id))
+    .catch((error) => handleRes(error.message));
+  return post;
+}
+
+function handleRes(data) {
+  let p = document.createElement("p");
+  p.textContent = data;
+
+  document.querySelector("body").appendChild(p);
+}
